a8f966bd863ab580c67544a57b16e2738a92a5c9
Waseb-untwale <untwalewaseb@gmail.com>
2024-08-08 15:25:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const Router = require("./routes/useRouter");
+const fileUpload = require('express-fileupload')
+const categoryRouter = require("./routes/categoryRouter");
+const productRouter= require('./routes/productRouter')
+const uploadRouter = require('./routes/upload')
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload({
+  useTempFiles:true
+}))
+const PORT = process.env.PORT || 5000;
+
+
+
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname,  "build")));
+  res.sendFile(path.resolve(__dirname,"build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("SERVER IS RUNNING ...");
+});
+
+//Routes
+app.use("/user", Router);
+app.use("/api", categoryRouter)
+app.use("/api",uploadRouter)
+app.use("/api", productRouter)
+
+
+//connect mongoDB
+const URI = process.env.MONGODB_URL;
+
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
